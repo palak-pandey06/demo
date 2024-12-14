9584dba14adf448dfb71ee49a5c11af76a44a594
palak-pandey06 <pandeypalak95@gmail.com>
2024-12-14 20:05:31 +0530

    pattern

--- my program.py
+++ my program.py
@@ -1,4 +1,20 @@
 print("Hello World!")
 print("My name is Palak")
 print (6+9)
-print(Mac*6)
+print("Mac"*6)
+print()
+
+# Take the number of rows for the pattern from the user
+n = int(input("Enter the number of rows: "))
+
+# Loop through each row
+for i in range(1, n + 1):
+    # Print '*' i times
+    print('*' * i)
+# Take the number of rows for the pattern from the user
+n = int(input("Enter the number of rows: "))
+
+# Loop through each row
+for i in range(1, n + 1):
+    # Print '*' i times
+    print('*' * i)
